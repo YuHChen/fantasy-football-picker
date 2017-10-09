823c1468904e093e2924cfa2c5cc6c212fc2d73d
Yu Heng Chen <yuhchen21@gmail.com>
2017-10-09 00:08:52 -0400

    Refactor Config so all instances share configuration values using Borg pattern.

--- ffpicker/config/config.py
+++ ffpicker/config/config.py
@@ -1,22 +1,28 @@
 import json, logging
 
-class Config:
-    _DEFAULT_CONFIG = {
-        # schedules
-        'SCHEDULE_DATA_DIR' : 'ffpicker-data/schedules',
-        'SCHEDULE_RAW_DATA_DIR' : 'ffpicker-raw-data/schedules',
-        'SCHEDULE_URL' : ('http://www.nfl.com/ajax/scorestrip?'
-                          'season={}&seasonType={}&week={}'),
+class Config(object):
+    _shared_state = {
+        '_DEFAULT_CONFIG' : {
+            # schedules
+            'SCHEDULE_DATA_DIR' : 'ffpicker-data/schedules',
+            'SCHEDULE_RAW_DATA_DIR' : 'ffpicker-raw-data/schedules',
+            'SCHEDULE_URL' : ('http://www.nfl.com/ajax/scorestrip?'
+                              'season={}&seasonType={}&week={}'),
+        },
+        '_CONFIG_FILENAME' : 'config.json'
     }
 
-    _CONFIG_FILENAME = 'config.json'
+    def __new__(cls, *p, **k):
+        self = object.__new__(cls)
+        self.__dict__ = cls._shared_state
+        return self
 
     def __init__(self, logger=None):
         self._logger = logger or logging.getLogger(Config.__name__)
-        self._config = Config._DEFAULT_CONFIG
+        self._config = self._DEFAULT_CONFIG
         # override default config with user defined config
         try:
-            with open(Config._CONFIG_FILENAME, 'r') as config_file:
+            with open(self._CONFIG_FILENAME, 'r') as config_file:
                 user_defined_config = json.load(config_file)
                 self._config = { **self._config, **user_defined_config }
         except FileNotFoundError:
@@ -24,7 +30,7 @@ class Config:
         except json.decoder.JSONDecodeError:
             message_format = ('Invalid JSON formatting in `%s`, '
                               'using default configuration settings instead')
-            self._logger.warning(message_format, Config._CONFIG_FILENAME)
+            self._logger.warning(message_format, self._CONFIG_FILENAME)
 
     def get_property(self, property_name):
         return self._config.get(property_name)
--- test/config/test_config.py
+++ test/config/test_config.py
@@ -1,32 +1,50 @@
 from ffpicker.config import Config
 
-import os, unittest
+import logging, os, unittest
 
 class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.disabled = True
+        self.config = Config()
+
     def test_constructor_givenConfigFileDoesNotExist_shouldUseDefaultConfiguration(self):
-        Config._CONFIG_FILENAME = 'ffpicker-does-not-exist.json'
+        self.config._CONFIG_FILENAME = 'ffpicker-does-not-exist.json'
         config = Config()
         expected = 'ffpicker-data/schedules'
         actual = config.get_property('SCHEDULE_DATA_DIR')
         self.assertEqual(actual, expected)
 
     def test_constructor_givenInvalidConfigFile_shouldUseDefaultConfiguration(self):
-        Config._CONFIG_FILENAME = 'ffpicker-invalid.json'
-        with open(Config._CONFIG_FILENAME, 'w+') as config_file:
+        self.config._CONFIG_FILENAME = 'ffpicker-invalid.json'
+        with open(self.config._CONFIG_FILENAME, 'w+') as config_file:
             config_file.write("")
-        config = Config()
-        os.remove(Config._CONFIG_FILENAME)
+        config = Config(self.logger)
+        os.remove(config._CONFIG_FILENAME)
         expected = 'ffpicker-data/schedules'
         actual = config.get_property('SCHEDULE_DATA_DIR')
         self.assertEqual(actual, expected)
 
     def test_constructor_givenValidConfigFile_shouldUseUserDefinedConfiguration(self):
-        Config._CONFIG_FILENAME = 'ffpicker-valid.json'
-        with open(Config._CONFIG_FILENAME, 'w+') as config_file:
+        self.config._CONFIG_FILENAME = 'ffpicker-valid.json'
+        with open(self.config._CONFIG_FILENAME, 'w+') as config_file:
             config_file.write("{ \"SCHEDULE_DATA_DIR\" : \"success\" }")
         config = Config()
-        os.remove(Config._CONFIG_FILENAME)
+        os.remove(config._CONFIG_FILENAME)
         expected = 'success'
+        actual = config.get_property('SCHEDULE_DATA_DIR')
+        self.assertEqual(actual, expected)
+
+    def test_constructor_shouldCreateInstancesWithSharedState(self):
+        self.config._CONFIG_FILENAME = 'ffpicker-config.json'
+        with open(self.config._CONFIG_FILENAME, 'w+') as config_file:
+            config_file.write("{ \"SCHEDULE_DATA_DIR\" : \"first\" }")
+        config = Config()
+        with open(config._CONFIG_FILENAME, 'w') as config_file:
+            config_file.write("{ \"SCHEDULE_DATA_DIR\" : \"second\" }")
+        Config()
+        os.remove(config._CONFIG_FILENAME)
+        expected = 'second'
         actual = config.get_property('SCHEDULE_DATA_DIR')
         self.assertEqual(actual, expected)
 
